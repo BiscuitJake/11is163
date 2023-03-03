cd74ae2deb23a79fa57b3f7f36a19c5d1f3bff5c
Biscuit Jake <ten007199@gmail.com>
2023-03-04 00:00:09 +0700

    UPDATE v0.21 SHOP

--- scripts/script_main.js
+++ scripts/script_main.js
@@ -17,6 +17,9 @@ var e_lgn = dc.querySelector("#enter_login")
 var e_psw = dc.querySelector('#enter_password') 
 var e_sub = dc.querySelector('.submit_enter');
 
+var r_lgn = dc.querySelector("#reg_login")
+var r_psw = dc.querySelector("reg_password")
+
 
 
 let user_json;
@@ -74,17 +77,25 @@ function enter_button(){
   })
   .then(res => res.json())
   .then((json) => save_json_enter(json))
-  
 ;
 }
 
 function save_json_enter(json){
   user_json = json;
+  console.log("-----")
+  console.log(json)
+  console.log("-----")
+  if (json.message == "Invalid credentials"){
+    alert("НЕПРАВИЛЬНЫЕ ДАННЫЕ")
+  }
+  else
+  {
   localStorage.setItem('token',user_json.token)
   localStorage.setItem('id',user_json.id)
   console.log(user_json.id)
   console.log(user_json.token)
   document.location.href = "/pages/store_pages.html"
+  }
 }
 
 function save_json_reg(json){
